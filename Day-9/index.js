5a09991377616ebd65faa08b666d5d10efa747b4
Shambhavi Priya <103560532+priShambhavi18@users.noreply.github.com>
2024-08-05 14:47:19 +0530

    Add files via upload

---
+++ Day-9/index.js
@@ -0,0 +1,17 @@
+const jsConvert = require('js-convert-case');
+const { v4: uuidv4 } = require('uuid');
+
+let usersArray = [
+    { uname: "Shambhavi", email: "sam20@gmail.com", cartId: "" },
+    { uname: "steve jobs", email: "STevE@gmail.com", cartId: "" },
+    { uname: "smith jobs", email: "SmIth@gmail.com", cartId: "" },
+    { uname: "Jones", email: "JoneS@gmail.com", cartId: "" }
+];
+
+for (let item of usersArray) {
+    const random_uuid = uuidv4();
+    item['uname'] = jsConvert.toHeaderCase(item['uname']);
+    item['email'] = jsConvert.toLowerCase(item['email']);
+    item['cartId'] = jsConvert.toUpperCase(random_uuid);
+    console.log(item);
+}
